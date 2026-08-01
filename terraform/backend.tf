8a501c4b086e318ad0cc70b58adf96fce45b97db
Bhathiya Vicum <bathiyavicum0@gmail.com>
2026-08-02 00:29:08 +0530

    Update S3 backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,8 +2,8 @@
 
 terraform {
   backend "s3" {
-    bucket = "your-terraform-state-bucket-2024"
-    key = "jenkins-pipeline/dev/terraform.tfstate"
+    bucket = "project-terraform-state-bhathiya"
+    key = "dev/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
     use_lockfile = true
